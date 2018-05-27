5dfff0c3edb9f2e6abd4c21cbd45d5e5b68c5975
yuva4297 <yuvaammu3@gmail.com>
2018-05-27 23:19:56 +0530

    All programs are done

---
+++ Assignments/Week1/JavaScriptProgrammingChallenge/Factorial/5.js
@@ -0,0 +1,35 @@
+function calc(){
+    var n=document.getElementById("given").value;
+    if(n<0){
+         document.getElementById("output").innerHTML="enter the positive number";
+    }
+    var out='';
+    for(var i=1;i<n;i++){
+        if(i%3==0 && i%5==0){
+            out=out+"FizzBuzz,"
+        }
+        else if(i%3==0){
+            out=out+"Fizz,"
+        }
+        else if(i%5==0){
+            out=out+"Buzz,"
+        }
+        else{
+            out=out+i+","
+        }
+    }
+    if(n%3==0 && n%5==0){
+            out=out+"FizzBuzz"
+        }
+        else if(n%3==0){
+            out=out+"Fizz"
+        }
+        else if(n%5==0){
+            out=out+"Buzz"
+        }
+        else{
+            out=out+i
+        }
+    //console.log(out);   
+    document.getElementById("output").innerHTML=out;
+}
